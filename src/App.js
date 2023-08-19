a9cf1cc8c59ac1a61b8e9018749df648c8b8109d
Mike Boyle <mikesboyle@gmail.com>
2023-08-19 18:16:59 -0400

    retrofit loading state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState({});
 
@@ -31,20 +32,29 @@ function App() {
   const API_URL = 'https://px32id5fdg.execute-api.us-east-1.amazonaws.com';
 
   const fetchData = async () => {
+    setLoading(true);
     const res = await fetch(`${API_URL}/items`);
     const json = await res.json();
     const { data } = json;
     setItems(data);
+    setLoading(false);
   };
   useEffect(() => {
     fetchData();
   }, []);
 
+  const renderLoadingOrMenu = () => {
+    if (loading) {
+      return <div className="App--loading">Loading...</div>
+    }
+    return <Menu handleAddToOrder={handleAddToOrder} items={items} />;
+  }
+
   return (
     <div className="App">
       <h1>Our Menu</h1>
       <Grid className="App__menu-grid">
-        <Menu handleAddToOrder={handleAddToOrder} items={items} />
+        {renderLoadingOrMenu()}
         <Order
           orderItems={orderItems}
           handleRemoveFromOrder={handleRemoveFromOrder}
